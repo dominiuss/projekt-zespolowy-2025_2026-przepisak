4d5c839f5e5fa1ea5f3fe61881a5d653ccc21b6e
Grodzicki Filip <filip.grodzicki@student.wat.edu.pl>
2025-11-14 09:03:04 +0100

    Dodaj endpointy GET i PUT profilu użytkownika

---
+++ src/Features/UserProfiles/Application/DTOs/UserProfileDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace PrzepisakApi.src.Features.UserProfiles.Application.DTOs
+{
+    public class UserProfileDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}
